380d89cbc6664513fe547655cd7350b27f8a2dbd
Jabeen Cheema <jabeencheema@pursuit.org>
2019-02-02 19:15:37 -0500

    profile view kind of set up, should put label inside view, will come back to it

--- Quizzes/Quizzes/Controllers/ProfileViewController.swift
+++ Quizzes/Quizzes/Controllers/ProfileViewController.swift
@@ -10,21 +10,18 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    let profileView = ProfileView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+       profileView.backgroundColor = .white
+    self.view.addSubview(profileView)
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
--- Quizzes/Quizzes/Views/ProfileView.swift
+++ Quizzes/Quizzes/Views/ProfileView.swift
@@ -10,12 +10,87 @@ import UIKit
 
 class ProfileView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    lazy var editPhotoLabel: UILabel = {
+        let editPhotoLabel = UILabel()
+        editPhotoLabel.text = "Edit Photo"
+        editPhotoLabel.backgroundColor = #colorLiteral(red: 1, green: 0.8602354673, blue: 0.8851751726, alpha: 1)
+        return editPhotoLabel
+    }()
+    
+    lazy var view: UIView = {
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 1, green: 0.9453986005, blue: 0.7692849898, alpha: 1)
+        return view
+    }()
+
+    lazy var profileButton: UIButton = {
+        let profileButton = UIButton()
+        profileButton.setBackgroundImage(UIImage(named: "placeholder-image"), for: .normal)
+        profileButton.backgroundColor = #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)
+        return profileButton
+    }()
+
+    lazy var usernameLabel: UILabel = {
+        let usernameLabel = UILabel()
+        usernameLabel.text = "username"
+        return usernameLabel
+        }()
+
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        //addSubview(editPhotoLabel)
+        //addSubview(view)
+        //addSubview(profileButton)
+       // addSubview(usernameLabel)
+        setEditPhotoLabel()
+        setViewConstraints()
+        setProfileButtonConstraints()
+        setusernameLabel()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func setEditPhotoLabel() {
+        addSubview(editPhotoLabel)
+        editPhotoLabel.translatesAutoresizingMaskIntoConstraints = false
+        editPhotoLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 120).isActive = true
+        editPhotoLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 11).isActive = true
+        editPhotoLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        editPhotoLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -11).isActive = true
+        //editPhotoLabel.widthAnchor.constraint(equalToConstant: 12).isActive = true
+    }
+    private func setViewConstraints() {
+         addSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.topAnchor.constraint(equalTo: editPhotoLabel.bottomAnchor, constant: 30).isActive = true
+        view.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
+        view.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
+        view.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor, multiplier: 0.50).isActive = true
+       
+    }
+
+    private func setProfileButtonConstraints() {
+         addSubview(profileButton)
+        profileButton.translatesAutoresizingMaskIntoConstraints = false
+        profileButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        profileButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        profileButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.50).isActive = true
+        profileButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.60).isActive = true
+      
+    }
+
+    private func setusernameLabel() {
+         addSubview(usernameLabel)
+        usernameLabel.translatesAutoresizingMaskIntoConstraints = false
+        usernameLabel.topAnchor.constraint(equalTo: view.bottomAnchor, constant: 30).isActive = true
+        usernameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 11).isActive = true
+        usernameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -11)
+        usernameLabel.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor, multiplier: 20)
+        
+       
+        
+    }
 
 }

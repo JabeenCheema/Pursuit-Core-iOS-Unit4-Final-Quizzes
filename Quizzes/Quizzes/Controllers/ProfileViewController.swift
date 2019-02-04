d68124bae292aa6e9e9cb368d4098c1294edae51
Jabeen Cheema <jabeencheema@pursuit.org>
2019-02-04 07:22:55 -0500

    able to upload a picture from photoLibrary and have the action sheet working for profile

--- Quizzes/Quizzes/Controllers/ProfileViewController.swift
+++ Quizzes/Quizzes/Controllers/ProfileViewController.swift
@@ -18,8 +18,11 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        profileView.backgroundColor = .white
-    self.view.addSubview(profileView)
+        self.view.addSubview(profileView)
         self.profileView.profileButton.addTarget(self, action: #selector(setImageAlert), for: .touchUpInside)
+        userDefault()
+        setUpAlertController()
+//        imagePickerController.delegate = self
         
         
         
@@ -48,4 +51,58 @@ class ProfileViewController: UIViewController {
     
     }
 
+    func setUpAlertController() {
+        let alertController = UIAlertController(title: "Enter your username", message: "", preferredStyle: .alert)
+        alertController.addTextField { (textfield) in
+            textfield.font = UIFont.boldSystemFont(ofSize: 17)
+            textfield.layer.cornerRadius = 7.0
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (alert: UIAlertAction) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        let submit = UIAlertAction(title: "Submit", style: .default) { (alert) in
+            guard let userName = alertController.textFields?.first?.text
+                else {
+                    return
+            }
+            self.profileView.usernameLabel.text = userName
+            
+            UserDefaults.standard.set(userName, forKey: UserDefaultKeys.DefaultSearchKey)
+        }
+        alertController.addAction(cancel)
+        alertController.addAction(submit)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func userDefault() {
+        if let defaultUsername = UserDefaults.standard.object(forKey: UserDefaultKeys.DefaultSearchKey) as? String {
+            
+            
+            self.profileView.profileButton.addTarget(self, action: #selector(setImageAlert), for: .touchUpInside)
+            self.profileView.usernameLabel.text = defaultUsername
+           
+        } else {
+            print("User Deafult not working")
+        }
+    }
+    
 }
+
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage { // i need to undertstand when to use guard and if let and if
+            profileView.profileButton.setBackgroundImage(image, for: .normal)
+        } else {
+            print ("image is not working")
+        }
+    dismiss(animated: true, completion: nil)
+    }
+}
+    
+    
+    
+
